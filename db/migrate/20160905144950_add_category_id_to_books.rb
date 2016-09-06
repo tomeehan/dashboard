6620b0daeb800f9de6201ddb39c28b9cc3f494e4
tomeehan <tom.meehan@me.com>
2016-09-06 01:47:43 +0100

    removied category_id from db/migrate

--- db/migrate/20160905144950_add_category_id_to_books.rb
+++ db/migrate/20160905144950_add_category_id_to_books.rb
@@ -1,5 +1,4 @@
 class AddCategoryIdToBooks < ActiveRecord::Migration[5.0]
   def change
-    add_column :posts, :category_id, :integer
   end
 end
--- db/migrate/20160905145341_add_category_id_to_posts.rb
+++ db/migrate/20160905145341_add_category_id_to_posts.rb
@@ -1,5 +1,4 @@
 class AddCategoryIdToPosts < ActiveRecord::Migration[5.0]
   def change
-    add_column :posts, :category_id, :integer
   end
 end
