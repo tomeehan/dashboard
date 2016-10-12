5ab9dc2ac28ebe883149bec3a614ce22eb75e076
tomeehan <tom.meehan@me.com>
2016-10-12 15:39:45 +0100

    fixed anchor hover problem

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -30,7 +30,7 @@ class GoalsController < ApplicationController
     respond_to do |format|
       if @goal.save
         format.html { redirect_to @goal, notice: 'Goal was successfully created.' }
-        format.json { render :show, status: :created, location: @goal }
+        format.json { render :@user.show, status: :created, location: @goal }
       else
         format.html { render :new }
         format.json { render json: @goal.errors, status: :unprocessable_entity }
