a8550e3a27b7f86d8eb37e20e2b1abaa304d8f2c
tomeehan <tom.meehan@me.com>
2016-11-04 12:58:08 +0000

    addded redirect to user show page when creating goal

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -29,7 +29,7 @@ class GoalsController < ApplicationController
 
     respond_to do |format|
       if @goal.save
-        format.html { redirect_to @goal, notice: 'Goal was successfully created.' }
+        format.html { redirect_to user_path(@goal.user.id), notice: 'Goal was successfully created.' }
         format.json { render :@user.show, status: :created, location: @goal }
       else
         format.html { render :new }
