f1d2c1bb4467bc7cf7da70922004223fc3afcf7e
tomeehan <tom.meehan@me.com>
2016-11-04 20:11:52 +0000

    team names - order by

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,10 @@ class UsersController < ApplicationController
 
 
   def show
-  	@user = User.find(params[:id])
+  	@user = User.find(params[:id]).order("name ASC")
 	@posts = @user.posts.order("created_at DESC")
 	@snippets = @user.snippets.order("created_at DESC")
 	@goals = @user.goals.order("created_at DESC")
+	@user
   end
 end
