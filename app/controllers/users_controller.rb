1e57c36b92f418f940e0de983494cb2a46787b80
tomeehan <tom.meehan@me.com>
2016-11-04 20:13:33 +0000

    fixed order by bug on user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,9 @@ class UsersController < ApplicationController
 
 
   def show
-  	@user = User.find(params[:id]).order("name ASC")
+  	@user = User.find(params[:id])
 	@posts = @user.posts.order("created_at DESC")
 	@snippets = @user.snippets.order("created_at DESC")
 	@goals = @user.goals.order("created_at DESC")
-	@user
   end
 end
